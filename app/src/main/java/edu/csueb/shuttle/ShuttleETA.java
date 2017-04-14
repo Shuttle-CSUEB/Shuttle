52d57007ee6ec6fe4e57e4a9606c7316d88a17e5
mwangxx0129 <mwangxx@gmail.com>
2017-04-13 18:02:25 -0700

    4 test

--- app/src/main/java/edu/csueb/shuttle/ShuttleETA.java
+++ app/src/main/java/edu/csueb/shuttle/ShuttleETA.java
@@ -65,7 +65,7 @@ public class ShuttleETA extends AppCompatActivity
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
-
+        //4/13/2017
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
             return true;
